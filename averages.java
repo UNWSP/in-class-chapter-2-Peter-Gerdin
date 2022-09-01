0a2dbb14442caa5e6c263fc5b23df483b9c116e0
Peter-Gerdin <Peterdavisgerdin@gmail.com>
2022-09-01 18:22:23 -0500

    Calculate and print the average of five students in the class

---
+++ averages.java
@@ -0,0 +1,21 @@
+// Peter Gerdin, Sep 1, 2022
+//  print the average of five students in the class
+
+
+public class averages {
+
+    public static void main(String[] args){
+        //-------------------------------------------------------------
+        double zach = 70;
+        double jack = 69;
+        double kiera = 60;
+        double peter = 72;
+        double hayden = 68;
+
+        double sum = zach + jack + kiera + peter + hayden;
+        double average = sum/5;
+
+        System.out.println("The average height is " + average + " inches");
+        //-------------------------------------------------------------
+    }
+}
